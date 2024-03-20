bff9d4d74aacde015c3e62e5844ad10eceba64cd
dmithamo <33679184+dmithamo@users.noreply.github.com>
2024-03-20 17:37:03 +0300

    settle on a logo design :/

--- app/components/app-logo.tsx
+++ app/components/app-logo.tsx
@@ -1,13 +1,42 @@
+import clsx from "clsx";
 import Link from "next/link";
 import GHIcon from "../../public/images/github-alt.svg";
 
-export const AppLogo: React.FC = () => (
-  <Link
-    href={"/"}
-    className="flex select-none gap-2 items-center uppercase font-bold text-accent"
-  >
-    <GHIcon fill="currentColor" className="h-5 w-5" />
-    <span className="opacity-65 text-fore">{"///"}</span>
-    <span className="text-accent">GH Stats</span>
-  </Link>
-);
+export enum ELogoSize {
+  small = "small",
+  large = "large",
+}
+
+type Props = {
+  size?: ELogoSize;
+};
+
+const logoSizeClasses = (
+  size?: ELogoSize
+): { iconSize: string; textSize: string } => {
+  switch (size) {
+    case ELogoSize.small:
+      return { iconSize: "h-4 w-4", textSize: "text-sm" };
+    case ELogoSize.large:
+      return { iconSize: "h-16 w-16", textSize: "text-4xl" };
+    default:
+      return { iconSize: "h-7 w-7", textSize: "text-lg" };
+  }
+};
+
+export const AppLogo: React.FC<Props> = ({ size }) => {
+  const { textSize, iconSize } = logoSizeClasses(size);
+  return (
+    <Link
+      href={"/"}
+      className={clsx(
+        "flex select-none gap-2 items-center uppercase font-bold text-accent",
+        textSize
+      )}
+    >
+      <GHIcon fill="currentColor" className={clsx(iconSize)} />
+      <span className="opacity-65 text-fore">{"///"}</span>
+      <span className="text-accent">GH Stats</span>
+    </Link>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { AppLogo, ELogoSize } from "./components/app-logo";
+
 export default function HomePage() {
   return (
-    <div className="h-full w-full flex items-center justify-center">
-      Hello, world!
+    <div className="h-[85vh] w-full flex items-center justify-center">
+      <AppLogo size={ELogoSize.large} />
     </div>
   );
 }
